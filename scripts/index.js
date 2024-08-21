2c84e892585c9ac1a8adf63e20f679199157f5cd
Franchesca Fatima Zelaya Contreras <fatimapolgar2001@gmail.com>
2024-08-21 15:57:32 -0500

    save

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,20 @@
-// Busquemos el formulario en el DOM
-let formElement = // Utiliza el método querySelector()
+const popupContainer = document.querySelector("#popup-profile");
+const profileButton = document.querySelector(".profile__edit-button");
+const profileName = document.querySelector(".profile__name");
+const profileHobbie = document.querySelector(".profile__hobbie");
+const inputName = document.querySelector("#input-name");
+const inputHobbie = document.querySelector("#input-hobbie");
+const formProfile = document.querySelector("#form__profile");
+const closeButton = document.querySelector(".form__close-button");
 
-// Lo siguiente es el manipulador (handler) de entrega de formularios, aunque
-// no se enviará en ningún sitio todavía
-
-// Observa que el nombre de la función comienza con un verbo
-// y describe exactamente lo que hace la función
-function handleProfileFormSubmit(evt) {
-    // Esta línea impide que el navegador
-    // entregue el formulario en su forma predeterminada.
-    evt.preventDefault();
-    // Una vez hecho esto, podemos definir nuestra propia forma de entregar el formulario.
-    // Lo explicaremos todo con más detalle después.
-
-    // Busquemos los campos del formulario en el DOM
-    let nameInput = // Utiliza el método querySelector()
-    let jobInput = // Utiliza el método querySelector()
-
-    // Obtén los valores de cada campo desde la propiedad de valor correspondiente
-
-    // Selecciona los elementos donde se introducirán los valores de los campos
-
-    // Inserta nuevos valores utilizando el textContent
-    // propiedad del método querySelector()
+function handleOpenProfile(evt) {
+  popupContainer.classList.add("popup__opened");
 }
 
-// Conecta el manipulador (handler) al formulario:
-// se observará el evento de entrega
-formElement.addEventListener('submit', handleProfileFormSubmit);
+function handleCloseProfile(evt) {
+  popupContainer.classList.remove("popup__opened");
+}
+
+profileButton.addEventListener("click", handleOpenProfile);
+
+closeButton.addEventListener("click", handleCloseProfile);
