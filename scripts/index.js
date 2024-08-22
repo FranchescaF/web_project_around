970382f35236cae3c21ae2a3d91bddbe96a37b7f
Franchesca Fatima Zelaya Contreras <fatimapolgar2001@gmail.com>
2024-08-21 22:58:54 -0500

    save

--- scripts/index.js
+++ scripts/index.js
@@ -4,17 +4,26 @@ const profileName = document.querySelector(".profile__name");
 const profileHobbie = document.querySelector(".profile__hobbie");
 const inputName = document.querySelector("#input-name");
 const inputHobbie = document.querySelector("#input-hobbie");
-const formProfile = document.querySelector("#form__profile");
+const formProfile = document.querySelector("#form-profile");
 const closeButton = document.querySelector(".form__close-button");
 
 function handleOpenProfile(evt) {
-  popupContainer.classList.add("popup__opened");
+  console.log(popupContainer);
+  popupContainer.classList.add("popup__show");
 }
 
 function handleCloseProfile(evt) {
-  popupContainer.classList.remove("popup__opened");
+  popupContainer.classList.remove("popup__show");
 }
 
+console.log(profileButton);
 profileButton.addEventListener("click", handleOpenProfile);
 
 closeButton.addEventListener("click", handleCloseProfile);
+
+formProfile.addEventListener("submit", function (evt) {
+  evt.preventDefault(); // Evita que el formulario se envíe de forma predeterminada
+  profileName.textContent = inputName.value; // Actualiza el nombre del perfil
+  profileHobbie.textContent = inputHobbie.value; // Actualiza el hobbie del perfil
+  handleCloseProfile(); // Cierra el popup después de actualizar el perfil
+});
