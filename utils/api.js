6dfba264c6d6837935f3c0494d9b7ebf71f0733d
Franchesca Fatima Zelaya Contreras <fatimapolgar2001@gmail.com>
2025-01-31 13:35:22 -0500

    save

--- utils/api.js
+++ utils/api.js
@@ -4,37 +4,77 @@ class Api {
     this.baseUrl = options.baseUrl;
     this.headers = options.headers;
   }
-
+  // Obtener información del usuario
   getUserInfo() {
     return fetch(this.baseUrl + "/users/me", {
-      headers: {
-        authorization: "a0cc38d0-c3cd-4ee6-b868-90b1ee1b784d",
-      },
+      headers: this.headers,
     })
       .then((res) => {
         return res.json();
       })
+      .then((result) => {
+        console.log(result);
+        return result;
+      });
+  }
+  // Obtener las tarjetas iniciales
+  getInitialCards() {
+    return fetch(this.baseUrl + "/cards", {
+      headers: this.headers,
+    })
+      .then((res) => res.json())
       .then((result) => {
         console.log(result);
         return result;
       });
   }
 
-  getInitialCards() {
-    return fetch(this.baseUrl + "/cards", {
+  // Editar el perfil del usuario
+  updateUserProfile() {
+    return fetch(this.baseUrl + "/me", {
+      method: "PATCH",
       headers: {
-        authorization: "a0cc38d0-c3cd-4ee6-b868-90b1ee1b784d",
+        authorization: this.headers,
+        "Content-Type": "application/json",
       },
+      body: JSON.stringify({
+        name: "Marie Skłodowska Curie",
+        about: "Física y Química",
+      }),
     })
       .then((res) => {
+        if (!res.ok) {
+          return Promise.reject(`Error: ${res.status}`);
+        }
         return res.json();
       })
       .then((result) => {
-        console.log(result);
+        console.log("Perfil actualizado:", result);
         return result;
       });
   }
-  // otros métodos para trabajar con la API
+}
+  //Agregar una nueva tarjeta
+  addNewCard(name, link) {
+    return fetch(this.baseUrl + "/cards", {
+      method: "POST",
+      headers: {
+        authorization: this.headers,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: name,
+        link: link,
+      }),
+    })
+      .then((res) => {
+        if (!res.ok) {
+          return Promise.reject(`Error: ${res.status}`);
+        }
+        return res.json();
+      })
+      .catch((err) => console.error("Error al agregar la tarjeta:", err));
+  }
 }
 
 export const api = new Api({
